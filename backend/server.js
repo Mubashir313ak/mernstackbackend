cb1e3f5a471fd7a0f897adc7945a2438ce153701
Mubashir313ak <119850184+Mubashir313ak@users.noreply.github.com>
2024-05-19 13:56:10 -0700

    userModel Created

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const notes = require("./data/notes");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 const app = express();
 dotenv.config();
@@ -19,17 +20,14 @@ app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
 
-app.use("/api/users", userRoutes);
-// app.get("/api/notes/:id", (req, res) => {
-//   const note = notes.find((n) => n._id === req.params.id);
-//   res.send(note);
-// });
+app.use(express.json());
 
-// app.post("/notes/bakchodi", (req, res) => {
-//   const newNote = req.body;
-//   notes.push(newNote);
-//   res.json(notes);
-// });
+app.use("/api/users", userRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`server started at ${PORT} `));
